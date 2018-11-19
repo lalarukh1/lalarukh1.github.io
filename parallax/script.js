709701ee0fad1f6cc4fa0b73bc954bf5fd9b82d2
lalarukh1 <35193027+lalarukh1@users.noreply.github.com>
2018-11-19 11:29:51 +0000

    Update script.js

--- parallax/script.js
+++ parallax/script.js
@@ -4,7 +4,7 @@
         let wipeAnimation = new TimelineMax()
             .fromTo("section.panel.second", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
             .fromTo("section.panel.third",    1, {x:  "-120%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-            .fromTo("section.panel.fourth", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+            .fromTo("section.panel.fourth", 1, {y: "-120%"}, {y: "0%", ease: Linear.easeNone}); // in from top
         new ScrollMagic.Scene({
             triggerElement: "#pinContainer",
             triggerHook: "onLeave",
@@ -37,6 +37,38 @@
             offset: 400
         })
             .setTween(tween)
-            .addTo(controller_2)
-    });
+            .addTo(controller_2);
 
+        // Text Effects
+        var tween_2 = new TimelineMax();
+        tween_2.to($('.text'), 0.9, {y: '300%', ease:Linear.easeInOut});
+        var introScene = new ScrollMagic.Scene({
+            triggerElement: '.fourth',
+            triggerHook: 0,
+            offset: 1
+        })
+            .setTween(tween_2)
+            .addTo(controller);
+
+        //hover Effects
+        $(".movearea").mousemove(function(e) {
+            parallaxIt(e, ".move2", 25);
+            parallaxIt(e, ".move3", 25);
+            parallaxIt(e, ".move4", 25);
+            parallaxIt(e, "img", 20);
+        });
+        $(".move1").mousemove(function(e) {
+            parallaxIt(e, ".move1", 50);
+        });
+        function parallaxIt(e, target, movement) {
+            var $this = $(".movearea");
+            var $this = $(".move1");
+            var relX = e.pageX - $this.offset().left;
+            var relY = e.pageY - $this.offset().top;
+
+            TweenMax.to(target, 1, {
+                x: (relX - $this.width() / 5) / $this.width() * movement,
+                y: (relY - $this.height() / 5) / $this.height() * movement
+            });
+        }
+    });
