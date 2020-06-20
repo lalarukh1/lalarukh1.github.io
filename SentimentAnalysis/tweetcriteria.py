0856e3c2250549601af85c7624a3068e7165e9a0
lalarukh1 <35193027+lalarukh1@users.noreply.github.com>
2020-06-20 13:15:02 +0100

    Create tweetcriteria.py

---
+++ SentimentAnalysis/tweetcriteria.py
@@ -0,0 +1,22 @@
+class TweetCriteria:
+
+    def __init__(self):
+        self.maxTweets = 0
+        self.topTweets = False
+        self.within = "15mi"
+
+    def setSince(self, since):
+        self.since = since
+        return self
+
+    def setUntil(self, until):
+        self.until = until
+        return self
+
+    def setQuerySearch(self, querySearch):
+        self.querySearch = querySearch
+        return self
+
+    def setLang(self, Lang):
+        self.lang = Lang
+        return self
